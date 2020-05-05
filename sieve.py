fab734d63db983cc18bdadb43c27e7608981a473
Mona <bv237@stud.uni-heidelberg.de>
2020-05-05 21:00:06 +0200

    Wirsing-star

--- sieve.py
+++ sieve.py
@@ -2,8 +2,7 @@ import math
 
 def sieb(n):
      #wir gehen davon aus: alle Zahlen sind PZ
-     #Länge n+1, denn: wir wollen PZ bis n nicht n-1
-    feld = [True]*(n+1) 
+    feld = [True]*n 
     primfeld = list()
     for i in range(2,n):
         if feld[i]==True:
