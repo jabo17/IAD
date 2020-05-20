2f3ec0b441c8c4394dedd6163abc87e7f2ce54bf
Jannick Borowitz <jannick.borowitz@gmail.com>
2020-05-20 17:38:36 +0200

    archimedes

--- uebung03/archimedes_jannick.py
+++ uebung03/archimedes_jannick.py
@@ -18,6 +18,7 @@ def archimedes1(k):
         t_2n = (2/t_2n)*(sqrt(4+t_2n**2)-2)
         n *= 2
         print("n:", n, "i:", i, "pi:", (n*(s_2n+t_2n))/4, "diff:", t_2n-s_2n, sep=" ")
+    return (n, (n*(s_2n+t_2n))/4, s_2n, t_2n)
 
 def archimedes2(k):
     '''
@@ -33,18 +34,29 @@ def archimedes2(k):
         #Rundungsfehler pflanzen sich in der Iteration fort
         #Dadurch wird t_2n unsinnigerweise groesser als s_2n aber einem gewissen anzahl von verdopplungen   
         s_2n = s_2n/(sqrt(2+sqrt(4-s_2n**2)))
-        t_2n = (2*s_2n)/sqrt(4-s_2n**2)
-        #t_2n = (2*t_2n)/(sqrt(4+t_2n**2)+2)
+        #t_2n = (2*s_2n)/sqrt(4-s_2n**2)
+        t_2n = (2*t_2n)/(sqrt(4+t_2n**2)+2)
         n *= 2
         print("n:", n, "i:", i, "pi:", (n*(s_2n+t_2n))/4, "diff:", t_2n-s_2n, sep=" ")
+    return (n, (n*(s_2n+t_2n))/4, s_2n, t_2n)
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def k():
-    return 60
+    return 29
 
-def test_archimedes1(k):
-    archimedes1(k)
+@pytest.fixture(scope="module")
+def t_n(k):
+    s_2n = sqrt(2)
+    for i in range(k):
+        s_2n = s_2n/(sqrt(2+sqrt(4-s_2n**2)))
+    return (2*s_2n)/sqrt(4-s_2n**2)
 
-def test_archimedes2(k):
-    #der output von archmides2 dient als verfikation
-    archimedes2(k)
+
+def test_archimedes1(k, t_n):
+    n, pi, s, t = archimedes1(k)
+    assert t_n == t
+
+def test_archimedes2(k, t_n):
+    print(t_n)
+    n, pi, s, t = archimedes2(k)
+    assert t_n == t
