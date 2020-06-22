9eb6ee416069aea2de1d793c438d861242ac6189
Jannick Borowitz <jannick.borowitz@gmail.com>
2020-06-22 20:03:59 +0200

    uebung07 init

---
+++ uebung07/cocktails_jannick.py
@@ -0,0 +1,27 @@
+"""
+ AlDa SS 20 - Uni Heidelberg
+ Implementierung der cocktails.py von Jannick
+ E-Mail: ak238@stud.uni-heidelberg.de
+"""
+import pytest
+
+FILE_COCKTAILS_INVERSE = "cocktails_inverse_jannick.json"
+
+
+def all_ingredients(recipes):
+    ...
+
+
+def normalize_strings(s):
+    # TODO use string patterns to normalize strings
+    ...
+    return s
+
+
+def cocktails_inverse(recipes):
+    inverse_recipes = dict()
+    # TODO reverse recipes
+    ...
+    # TODO export inverse_recipes into FILE_COCKTAILS_INVERSE
+    ...
+    return inverse_recipes
